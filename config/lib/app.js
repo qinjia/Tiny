f79d95ac8247dcd148c5aeae52a82a0eb2c54e12
penguin3069 <penguin3069@163.com>
2015-10-12 10:33:41 +0800

    Tiny
    初始化

---
+++ config/lib/app.js
@@ -0,0 +1,67 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var config = require('../config'),
+  // mongoose为mongodb的链接包
+  mongoose = require('./mongoose'),
+  express = require('./express'),
+  chalk = require('chalk'),
+  seed = require('./seed');
+
+function seedDB() {
+  if (config.seedDB) {
+    console.log(chalk.bold.red('Warning:  Database seeding is turned on'));
+    seed.start();
+  }
+}
+
+// Initialize Models
+mongoose.loadModels(seedDB);
+
+
+// module.exports是暴露给外部引用的接口，其内的所有属性和方法都可以由外部使用
+module.exports.loadModels = function loadModels() {
+  mongoose.loadModels();
+};
+
+module.exports.init = function init(callback) {
+  mongoose.connect(function (db) {
+    // Initialize express
+    var app = express.init(db);
+    if (callback) callback(app, db, config);
+
+  });
+};
+
+
+// 这个方法就是程序的入口方法
+module.exports.start = function start(callback) {
+  var _this = this;
+
+  _this.init(function (app, db, config) {
+
+    // Start the app by listening on <port>
+    app.listen(config.port, function () {
+
+      // Logging initialization
+      console.log('--');
+      console.log(chalk.green(config.app.title));
+      console.log(chalk.green('Environment:\t\t\t' + process.env.NODE_ENV));
+      console.log(chalk.green('Port:\t\t\t\t' + config.port));
+      console.log(chalk.green('Database:\t\t\t\t' + config.db.uri));
+      if (process.env.NODE_ENV === 'secure') {
+        console.log(chalk.green('HTTPs:\t\t\t\ton'));
+      }
+      console.log(chalk.green('App version:\t\t\t' + config.meanjs.version));
+      if (config.meanjs['meanjs-version'])
+        console.log(chalk.green('MEAN.JS version:\t\t\t' + config.meanjs['meanjs-version']));
+      console.log('--');
+
+      if (callback) callback(app, db, config);
+    });
+
+  });
+
+};
---
+++ server.js
@@ -0,0 +1,8 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+// 程序入口为app.start,/config/lib/app.js中的start方法
+var app = require('./config/lib/app');
+var server = app.start();
